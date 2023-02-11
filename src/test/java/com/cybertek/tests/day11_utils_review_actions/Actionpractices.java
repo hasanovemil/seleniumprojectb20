c69074f19b3d5966741d84ee19866320a5665d80
hasanovemil <emil-2035@mail.ru>
2023-02-11 13:28:23 +0400

    a

--- src/test/java/com/cybertek/tests/day11_utils_review_actions/Actionpractices.java
+++ src/test/java/com/cybertek/tests/day11_utils_review_actions/Actionpractices.java
@@ -1,4 +1,23 @@
 package com.cybertek.tests.day11_utils_review_actions;
 
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
 public class Actionpractices {
+
+
+    @Test
+    public void hover_over_test(){
+
+        Driver.getDriver().get("http://practice.cybertekschool.com/hovers");
+
+        WebElement img1 = Driver.getDriver().findElement(By.xpath("(//div[@class='figure'])[1]"));
+
+
+
+    }
+
+
 }
