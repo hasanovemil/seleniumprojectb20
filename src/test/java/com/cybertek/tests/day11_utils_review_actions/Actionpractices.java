a9abcf6813e5e14622a40a79af3d14d0dab0d168
hasanovemil <emil-2035@mail.ru>
2023-02-11 14:01:40 +0400

    a

--- src/test/java/com/cybertek/tests/day11_utils_review_actions/Actionpractices.java
+++ src/test/java/com/cybertek/tests/day11_utils_review_actions/Actionpractices.java
@@ -1,8 +1,11 @@
 package com.cybertek.tests.day11_utils_review_actions;
 
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Actionpractices {
@@ -20,6 +23,11 @@ public class Actionpractices {
         WebElement user1 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user1']"));
         WebElement user2 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user2']"));
         WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user3']"));
+
+        Actions actions = new Actions(Driver.getDriver());
+        BrowserUtils.wait(2);
+        actions.moveToElement(img1).perform();
+        Assert.assertTrue(user1.isDisplayed(),"Img1 is not displayed.Verification failed!");
 
 
 
