09fcab080d5771691d8598478a18224f7c57a82f
hasanovemil <emil-2035@mail.ru>
2023-02-12 00:27:22 +0400

    a

---
+++ src/test/java/com/cybertek/tests/day12_review_jsexecutor_pom/ActionsPractices.java
@@ -0,0 +1,11 @@
+package com.cybertek.tests.day12_review_jsexecutor_pom;
+
+import org.testng.annotations.Test;
+
+public class ActionsPractices {
+
+    @Test
+    public void p2_double_click_test(){
+
+    }
+}
