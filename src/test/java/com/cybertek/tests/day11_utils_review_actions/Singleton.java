7ed67e0a1a75a090a94cbccb0eed6fa0e098a12d
hasanovemil <emil-2035@mail.ru>
2023-02-11 11:52:16 +0400

    a

--- src/test/java/com/cybertek/tests/day11_utils_review_actions/Singleton.java
+++ src/test/java/com/cybertek/tests/day11_utils_review_actions/Singleton.java
@@ -2,9 +2,13 @@ package com.cybertek.tests.day11_utils_review_actions;
 
 public class Singleton {
 
-    private Singleton(){
+    //Making the constructor private, so we limit creating objects from this class
+    private Singleton(){}
 
-
+    //Making the String private so that we can only reach this using getter method
+    private static String word;
+    //
+    public static String getWord(){
 
 
     }
