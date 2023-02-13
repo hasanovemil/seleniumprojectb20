8fc38c84fce55c965afa351f746c9bf3494f6586
hasanovemil <emil-2035@mail.ru>
2023-02-13 23:55:09 +0400

    a

--- src/test/java/com/cybertek/tests/day13_pom_synchronization/Synchronization_Practices.java
+++ src/test/java/com/cybertek/tests/day13_pom_synchronization/Synchronization_Practices.java
@@ -1,5 +1,19 @@
 package com.cybertek.tests.day13_pom_synchronization;
 
+import com.cybertek.utilities.Driver;
+import org.testng.annotations.Test;
+
 public class Synchronization_Practices {
+
+    @Test
+    public void p40_dynamic_title_test(){
+        String url = "http://practice.cybertekschool.com/dynamic_loading/7";
+        Driver.getDriver().get(url);
+
+
+
+
+
+    }
 
 }
