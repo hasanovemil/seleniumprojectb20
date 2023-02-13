8aa0560d3f411d6f7442950c443d437ae508b1c1
hasanovemil <emil-2035@mail.ru>
2023-02-14 00:37:57 +0400

    a

--- src/test/java/com/cybertek/pages/Loading7page.java
+++ src/test/java/com/cybertek/pages/Loading7page.java
@@ -1,15 +1,26 @@
 package com.cybertek.pages;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class Loading7page {
 
     public Loading7page(){
-
         PageFactory.initElements(Driver.getDriver(),this);
-
-
-
     }
+
+    @FindBy(id="alert")
+    public WebElement doneMessage;
+
+
+
+
+
+
+
+
+
+
 }
--- src/test/java/com/cybertek/tests/day13_pom_synchronization/Synchronization_Practices.java
+++ src/test/java/com/cybertek/tests/day13_pom_synchronization/Synchronization_Practices.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day13_pom_synchronization;
 
+import com.cybertek.pages.Loading7page;
 import com.cybertek.utilities.Driver;
 import org.testng.annotations.Test;
 
@@ -9,6 +10,11 @@ public class Synchronization_Practices {
     public void p40_dynamic_title_test(){
         String url = "http://practice.cybertekschool.com/dynamic_loading/7";
         Driver.getDriver().get(url);
+
+        Loading7page loading7page = new Loading7page();
+
+
+
 
 
 
